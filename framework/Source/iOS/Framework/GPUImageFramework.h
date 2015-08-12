cf066e033d1fe3a95439fe7328f391d2fd2c720d
Fernando Wong <f.wong@allm.net>
2015-08-12 17:58:46 +0900

    Adding four and eight input filters to GPUImageFramework.h

--- framework/Source/iOS/Framework/GPUImageFramework.h
+++ framework/Source/iOS/Framework/GPUImageFramework.h
@@ -170,3 +170,5 @@ FOUNDATION_EXPORT const unsigned char GPUImageFrameworkVersionString[];
 #import <GPUImage/GPUImageWeakPixelInclusionFilter.h>
 #import <GPUImage/GPUImageFASTCornerDetectionFilter.h>
 #import <GPUImage/GPUImageMovieComposition.h>
+#import <GPUImage/GPUImageFourInputFilter.h>
+#import <GPUImage/GPUImageEightInputFilter.h>
